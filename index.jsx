d51361d9624f225704c77fcceb80f55ef7eaa373
Nishanth Reddy Kamballapalli <S525727@mail.nwmissouri.edu>
2017-07-18 14:45:16 -0500

    Add functionality to buttons
    Added the functionality to the decrement and increment buttons.

--- index.jsx
+++ index.jsx
@@ -41,15 +41,22 @@ var Counter = React.createClass({
             score: 0,
         }
     },
-    inccrementScore: function(e){
-        console.log(e);
+    decrementScore: function () {
+        this.setState({
+            score: (this.state.score - 1),
+        });
+    },
+    incrementScore: function(){
+        this.setState({
+            score: (this.state.score + 1),
+        });
     },
     render: function () {
         return(
             <div className="counter">
-                <button className="counter-action decrement"> - </button>
+                <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
                 <div className="counter-score"> {this.state.score} </div>
-                <button className="counter-action increment" onClick={this.inccrementScore}> + </button>
+                <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
         );
     }
