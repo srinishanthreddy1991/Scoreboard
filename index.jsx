ddc54b7f27776df942bfaa90e00369c363572288
Nishanth Reddy Kamballapalli <S525727@mail.nwmissouri.edu>
2017-07-18 21:23:12 -0500

    Add stateless component stats
    Added stateless component to show the number of players in the Players array in the header.

--- index.jsx
+++ index.jsx
@@ -20,9 +20,34 @@ var PLAYERS = [
     }
 ]
 
+function Stats(props) {
+
+    var totalPlayers = props.players.length;
+
+    return(
+        <table className="stats">
+            <tbody>
+            <tr>
+                <td>Players:</td>
+                <td>{totalPlayers}</td>
+            </tr>
+            <tr>
+                <td>Total Points:</td>
+                <td>123</td>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
+
+Stats.propTypes = {
+    players: React.PropTypes.array.isRequired,
+};
+
 function Header(props) {
     return(
         <div className="header">
+            <Stats players={props.players}/>
             <h1>{props.title}</h1>
         </div>
     );
@@ -30,6 +55,7 @@ function Header(props) {
 
 Header.propTypes = {
     title: React.PropTypes.string.isRequired,
+    players: React.PropTypes.array.isRequired,
 };
 
 // var Counter = React.createClass({
@@ -65,15 +91,16 @@ Header.propTypes = {
 function Counter(props) {
     return(
         <div className="counter">
-            <button className="counter-action decrement"> - </button>
+            <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
             <div className="counter-score"> {props.score} </div>
-            <button className="counter-action increment"> + </button>
+            <button className="counter-action increment" onClick={function() {props.onChange(1);}}> + </button>
         </div>
     );
 }
 
 Counter.propTypes = {
     score: React.PropTypes.number.isRequired,
+    onChange: React.PropTypes.func.isRequired,
 }
 
 function Player(props) {
@@ -83,7 +110,7 @@ function Player(props) {
                 {props.name}
             </div>
             <div className="player-score">
-                <Counter initialScore={props.score}/>
+                <Counter score={props.score} onChange={props.onScoreChange}/>
             </div>
         </div>
     );
@@ -91,7 +118,8 @@ function Player(props) {
 
 Player.propTypes = {
     name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired
+    score: React.PropTypes.number.isRequired,
+    onScoreChange: React.PropTypes.func.isRequired,
 };
 
 
@@ -118,14 +146,26 @@ var Application = React.createClass({
         };
     },
 
+    onScoreChange: function (index, delta) {
+      console.log("onScoreChange",index, delta);
+      this.state.players[index].score += delta;
+      this.setState(this.state);
+    },
+
     render: function(){
         return(
             <div className="scoreboard">
-                <Header title={this.props.title}/>
+                <Header title={this.props.title} players={this.state.players}/>
                 <div className="players">
-                    {this.state.players.map(function (player) {
-                        return  <Player name={player.name} score={player.score} key={player.id}/>
-                    })}
+                    {this.state.players.map(function (player, index) {
+                        return (
+                            <Player
+                                onScoreChange={function(delta){this.onScoreChange(index,delta)}.bind(this)}
+                                name={player.name}
+                                score={player.score}
+                                key={player.id}/>
+                        );
+                    }.bind(this))}
                 </div>
             </div>
         );
