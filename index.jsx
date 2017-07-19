0082bec6f6b988b985872d6a0c18ca508e33ca50
Nishanth Reddy Kamballapalli <S525727@mail.nwmissouri.edu>
2017-07-18 21:42:40 -0500

    Add total number of points into ui
    Added the total number of points of players to the dom to show on page.

--- index.jsx
+++ index.jsx
@@ -23,6 +23,9 @@ var PLAYERS = [
 function Stats(props) {
 
     var totalPlayers = props.players.length;
+    var totalPoints = props.players.reduce(function (total, player) {
+       return total += player.score;
+    }, 0);
 
     return(
         <table className="stats">
@@ -33,7 +36,7 @@ function Stats(props) {
             </tr>
             <tr>
                 <td>Total Points:</td>
-                <td>123</td>
+                <td>{totalPoints}</td>
             </tr>
             </tbody>
         </table>
